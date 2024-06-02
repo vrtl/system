ac25c0b6f2015e159db3722f610557074b006ad0
Merlin Addams <merlin.addams@protonmail.com>
2024-06-02 13:08:28 +0100

    Updated deps

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,6 +1,11 @@
-use leptos::{mount_to_body, view};
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    view! { <p>"Hello, World!"</p> }
+}
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <p>"Hello, World!"</p> })
+    mount_to_body(|| view! { <App/> })
 }
